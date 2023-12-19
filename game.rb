1fba950e613da0c278dfcefd8792fceb62e5609a
Daniel <150309766+dgyoungk@users.noreply.github.com>
2023-12-18 20:07:58 -0800

    Add Game class to repo

---
+++ game.rb
@@ -0,0 +1,49 @@
+class Game
+  include Playable
+  include Displayable
+
+  attr_accessor :turns, :breaker, :creator, :feedback
+  def initialize
+    self.turns = 1
+    self.feedback = {}
+    setup_game()
+  end
+
+  def setup_game
+    welcome_msg()
+    new_player_msg()
+    player_name = gets.chomp
+    choice_msg()
+    role_selection = gets.chomp.to_i
+    designate_player(role_selection, player_name)
+    # Playable method
+    start_game(self)
+  end
+
+  def start_match
+    options_msg(self.creator.color_options)
+    self.breaker.guess.each_with_index do |(k, v), idx|
+      print %(Color at position #{idx + 1}: )
+      guess_color = gets.chomp.downcase
+      until self.creator.color_options.include?(guess_color)
+        puts %(That color is not an option, try again)
+        print %(Color at position #{idx + 1}: )
+        guess_color = gets.chomp.downcase
+      end
+      self.breaker.guess[k] = guess_color
+    end
+  end
+
+  def designate_player(role, player_name)
+    # 1 indicates that the player wants to be the codebreaker
+    if role == 1
+      self.breaker = Codebreaker.new(player_name)
+      self.creator = Mastermind.new
+    else
+      self.breaker = Codebreaker.new
+      self.creator = Mastermind.new(player_name)
+    end
+  end
+
+  private
+end
