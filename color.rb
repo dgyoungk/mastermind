fcb5afba878e5a383cc10b248bde2ff3ce9c0b84
Daniel <150309766+dgyoungk@users.noreply.github.com>
2024-01-11 22:17:10 -0800

    Add color module to repo

---
+++ color.rb
@@ -0,0 +1,18 @@
+module Color
+  def init_color_nums
+    color_nums = {
+      green: 1,
+      blue: 2,
+      red: 3,
+      yellow: 4,
+      orange: 5,
+      purple: 6
+    }
+    color_nums
+  end
+
+  def init_colors
+    colors_str = %w(green blue red yellow orange purple)
+    colors_str
+  end
+end
